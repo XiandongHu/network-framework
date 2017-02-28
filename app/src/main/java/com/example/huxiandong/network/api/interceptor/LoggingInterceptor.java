852d6d3022e35eaaefa167a28b85943a6d4d03ce
huxiandong <huxiandong@gmail.com>
2017-02-28 20:56:24 +0800

    log response body
    Signed-off-by: huxiandong <huxiandong@gmail.com>

--- app/src/main/java/com/example/huxiandong/network/api/interceptor/LoggingInterceptor.java
+++ app/src/main/java/com/example/huxiandong/network/api/interceptor/LoggingInterceptor.java
@@ -3,10 +3,15 @@ package com.example.huxiandong.network.api.interceptor;
 import com.example.huxiandong.network.api.logger.ApiLogger;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 import okhttp3.Interceptor;
+import okhttp3.MediaType;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
+import okio.Buffer;
+import okio.BufferedSource;
 
 /**
  * Created by huxiandong
@@ -14,6 +19,8 @@ import okhttp3.Response;
  */
 
 public class LoggingInterceptor implements Interceptor {
+
+    private static final Charset UTF8 = Charset.forName("UTF-8");
 
     private ApiLogger mLogger;
 
@@ -28,8 +35,24 @@ public class LoggingInterceptor implements Interceptor {
         mLogger.i("Sending request %s on %s\n%s", request.url(), chain.connection(), request.headers());
 
         Response response = chain.proceed(request);
-        long t2 = System.nanoTime();
-        mLogger.i("Received response for %s in %.1fms\n%s", request.url(), (t2 - t1) / 1e6d, response.headers());
+        ResponseBody body = response.body();
+        if (body != null && body.contentLength() > 0) {
+            BufferedSource source = body.source();
+            source.request(8192);
+            Buffer buffer = source.buffer();
+            Charset charset = UTF8;
+            MediaType contentType = body.contentType();
+            if (contentType != null) {
+                charset = contentType.charset(UTF8);
+            }
+            long t2 = System.nanoTime();
+            mLogger.i("Received response for %s in %.1fms\n%s", request.url(), (t2 - t1) / 1e6d, response.headers());
+            mLogger.i("Body <-- %s", buffer.clone().readString(charset));
+        } else {
+            long t2 = System.nanoTime();
+            mLogger.i("Received response for %s in %.1fms\n%s", request.url(), (t2 - t1) / 1e6d, response.headers());
+            mLogger.i("No Body <--");
+        }
 
         return response;
     }
